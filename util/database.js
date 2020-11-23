705604a72435770b9d8b0d55a20a9bbd83c95467
jsampayoramos <joao.sramos.84@gmail.com>
2020-11-23 20:07:33 +0000

    heroky deployment

--- util/database.js
+++ util/database.js
@@ -1,5 +1,22 @@
 import Sequelize from 'sequelize';
 
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'mysql',
+        protocol: 'mysql',
+        logging: true
+    });
+  } else {
+    // the application is executed on the local machine
+    sequelize = new Sequelize(process.env.SEQUELIZE_DATABASE, process.env.SEQUELIZE_USER, process.env.SEQUELIZE_PASSWORD, {
+        host: 'localhost',
+        dialect: 'mysql'
+    });
+  }
+
 const sequelize = new Sequelize(process.env.SEQUELIZE_DATABASE, process.env.SEQUELIZE_USER, process.env.SEQUELIZE_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql'
